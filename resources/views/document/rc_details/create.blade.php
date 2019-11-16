211fee6dd6e20854b614da05fe6b0698d394080b
somitra <somitra7@gmail.com>
2019-11-16 17:55:41 +0530

    add count on dashbord

--- resources/views/document/rc_details/create.blade.php
+++ resources/views/document/rc_details/create.blade.php
@@ -430,7 +430,7 @@
 		                        <div class="col-md-2 col-xl-2 mt-5">
 	                                <table class="table">
 	                                  <tr>
-	                                    <th><center>Road  Tax Image</center></th>
+	                                    <th><center>RC Image</center></th>
 	                                  </tr>
 	                                  <tr>
 	                                    <td>
